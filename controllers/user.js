40c37bddb70e6adf8bf49aff4a63c1a606659c1a
Irving Herrera <irving.herreramolina@gmail.com>
2020-05-26 16:14:12 -0500

    funcionalidad para guardar usuarios en db,  conectar con servicios

--- controllers/user.js
+++ controllers/user.js
@@ -3,7 +3,41 @@ const bcrypt = require('bcrypt-nodejs');
 const User = require('../models/user');
 
 function signUp(req, res) {
-    console.log('Endpoint SignUp');
+    const user = new User();
+
+    const { name, lastname, email, password, repeatPassword } = req.body;
+    user.name = name;
+    user.lastname = lastname;
+    user.email = email.toLowerCase();
+    user.role = "admin";
+    user.active = false;
+
+    if (!password || !repeatPassword ) {
+        res.status(404).send({ message: 'Las contraeñas son obligatorias' });
+    } else {
+        if(password !== repeatPassword) {
+            res.status(404).send({ message: 'Las contraeñas no son iguales ' });
+        } else {
+            bcrypt.hash(password, null, null, function(err, hash){
+                if(err) {
+                    res.status(500).send({ message: 'Erro al encriptar la contraseña' });
+                } else {
+                    user.password = hash;
+                    user.save((err, userStore) => {
+                        if(err) {
+                            res.status(500).send({ message: 'El usuario ya existe', erro: err });
+                        } else {
+                            if(!userStore) {
+                                res.status(404).send({ message: 'Erro al crear el usuario' });
+                            } else {
+                                res.status(200).send({ message: 'usuario creado', user: userStore });
+                            }
+                        }
+                    });
+                }
+            });
+        }
+    }
 }
 
 module.exports = {
