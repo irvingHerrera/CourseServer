21ec6dd9967b850f9ac43b52921f1bcbfa6d7437
Irving Herrera <irving.herreramolina@gmail.com>
2020-06-15 12:25:05 -0500

    endpoint get users

--- controllers/user.js
+++ controllers/user.js
@@ -75,7 +75,19 @@ function signIn(req, res) {
 
 }
 
+function getUsers(req, res) {
+    User.find().then(users => {
+        if(!users) {
+            res.status(404).send({ message: 'No se han encontrado usuarios' })
+        } else {
+            res.status(200).send({ users });
+        }
+    })
+    
+}
+
 module.exports = {
     signUp,
-    signIn
+    signIn,
+    getUsers
 };
