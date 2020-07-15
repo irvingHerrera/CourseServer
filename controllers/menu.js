2b85e8dffa34a13b17c2d65f053eb2fe9bf3a966
Irving Herrera <irving.herreramolina@gmail.com>
2020-07-14 20:22:04 -0500

    endpoint para obtener menus

--- controllers/menu.js
+++ controllers/menu.js
@@ -1,9 +1,38 @@
 const Menu = require('../models/menu');
 
 function addMenu(req, res) {
-    console.log('add menu');
+    const { title, url, order, active } = req.body;
+    const menu = new Menu();
+    menu.title = title;
+    menu.url = url;
+    menu.order = order;
+    menu.active = active;
+
+    menu.save((err, createMenu) => {
+        if(err) {
+            res.status(500).send({ message: 'Erro del servidor' });
+        } else {
+            if(!createMenu) {
+            res.status(404).send({ message: 'Erro al crear el menu' });
+            } else {
+            res.status(200).send({ message: 'Menu creado correctamente.' });
+            }
+        }
+    })
+}
+
+function getMenu(req, res) {
+    Menu.find().then(menus => {
+        if(!menus) {
+            res.status(404).send({ message: 'No se han encontrado los menus' })
+        } else {
+            res.status(200).send({ menus });
+        }
+    })
+    
 }
 
 module.exports = {
-    addMenu
+    addMenu,
+    getMenu
 };
--- routers/menu.js
+++ routers/menu.js
@@ -6,5 +6,6 @@ const md_auth = require('../middleware/authenticated');
 const api = express.Router();
 
 api.post('/addmenu', [md_auth.ensureAuth], MenuController.addMenu);
+api.get('/getMenu', [md_auth.ensureAuth], MenuController.getMenu);
 
 module.exports = api; 
